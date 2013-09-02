5d05da5d182f2f9f589766d820236a0dde806f93
grace Nefertiti T <grace.tchougbe@peergreen.com>
2013-09-02 11:22:07 +0200

    Remove uselless methods and add implementation for new method getPetals.

--- rest/src/main/java/com/peergreen/store/api/rest/resources/CapabilityOperations.java
+++ rest/src/main/java/com/peergreen/store/api/rest/resources/CapabilityOperations.java
@@ -1,39 +1,71 @@
 package com.peergreen.store.api.rest.resources;
 
+
+import java.util.Collection;
+import java.util.Iterator;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
 
-@Path(value="/capabilities")
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.peergreen.store.controller.IPetalController;
+import com.peergreen.store.db.client.ejb.entity.Petal;
+import com.peergreen.store.db.client.exception.NoEntityFoundException;
+
+@Path(value = "/capabilities")
 public class CapabilityOperations {
 
+    private IPetalController petalController;
+
     /**
-     * Retrieve petals which provide a given capability
-     * @param id the capability's id 
+     * @param petalController the petalController to set
      */
-     @GET
-    @Path("/{id}/petals")
-    public Response getPetals(@Context UriInfo uri, @PathParam(value = "id") int id){ 
-        //TODO method in the controller which retrieve a capability by his id 
-        
-        return null;
-    }    
-     
-     
-    @GET
-    @Path("/{id}")
-    public Response getCapability(){
-        //TODO method in the controller which retrieve a capability by his id 
-        return null; 
+    public void setPetalController(IPetalController petalController) {
+        this.petalController = petalController;
     }
-    
-     
+
+    /**
+     * Retrieve all petals satisfying a requirement.
+     * @param id the requirement's id
+     * @return All the petals existing which satisfy the given requirement
+     * @throws JSONException
+     */
     @GET
-    public Response getCapabilities(){
-        //TODO method in the controller to collect capabilities in the database 
-        return null; 
+    @Path("/{name}/{version}/petals")
+    public Response getPetals(@Context UriInfo uri, 
+            @PathParam(value = "name") String name,
+            @PathParam(value = "version") String version) throws JSONException {
+
+        JSONObject result = new JSONObject();
+        String path = uri.getBaseUri().toString();
+
+        try {
+            Collection<Petal> petals = petalController.
+                    getPetalsForCapability(name, version);
+            if (!petals.isEmpty()) {
+                Iterator<Petal> it = petals.iterator();
+                Petal p;
+                while (it.hasNext()) {
+                    p = it.next();
+                    result.put(p.getArtifactId(), path.concat("petal/"
+                            + p.getVendor() + "/" + p.getArtifactId()
+                            + "/" + p.getVersion()));
+                }
+                return Response.status(200).entity(result.toString()).build();
+            } else{
+                return Response.status(200).entity("No petal does not provide "
+                        + name).build();
+            }
+        } catch (NoEntityFoundException e) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
     }
+
 }
--- rest/src/main/java/com/peergreen/store/api/rest/resources/RequirementOperations.java
+++ rest/src/main/java/com/peergreen/store/api/rest/resources/RequirementOperations.java
@@ -1,6 +1,7 @@
 package com.peergreen.store.api.rest.resources;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,35 +9,61 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.peergreen.store.controller.IPetalController;
 import com.peergreen.store.db.client.ejb.entity.Petal;
+import com.peergreen.store.db.client.exception.NoEntityFoundException;
 
-@Path(value="/requirements")
+@Path(value="/requirement")
 public class RequirementOperations {
-/**
- * Retrieve all petals satisfying a requirement 
- * @param id the requirement's id 
- * @return All the petals existing which satisfy the given requirement 
- */
-    @GET
-    @Path("/{id}/petals")
-    public Collection<Petal> getPetals(@Context UriInfo uri, @PathParam(value = "id") int id){ 
-        // TODO method in the controller a requirement given his id 
-        return null;
-    }    
-    
-    @GET
-    @Path("/{id}")
-    public Response getRequirement(){
-    
-        // TODO method in the controller a requirement given his id 
-        return null; 
+
+    private IPetalController petalController;
+
+    /**
+     * @param petalController the petalController to set
+     */
+    public void setPetalController(IPetalController petalController) {
+        this.petalController = petalController;
     }
-    
+
+    /**
+     * Retrieve all petals satisfying a requirement.
+     * @param id the requirement's id
+     * @return All the petals existing which satisfy the given requirement
+     * @throws JSONException
+     */
     @GET
-    public Response getRequirements(){
-        
-        //TODO method in the controller to collect requirements in the database 
-        return null; 
+    @Path("/{name}/petals")
+    public Response getPetals(@Context UriInfo uri,
+            @PathParam(value = "name") String name) throws JSONException {
+
+        JSONObject result = new JSONObject();
+        String path = uri.getBaseUri().toString();
+
+        try {
+            Collection<Petal> petals = petalController.
+                    getPetalsForRequirement(name);
+            if (!petals.isEmpty()) {
+                Iterator<Petal> it = petals.iterator();
+                Petal p;
+                while (it.hasNext()) {
+                    p = it.next();
+                    result.put(p.getArtifactId(), path.concat("petal/"
+                            + p.getVendor() + "/" + p.getArtifactId()
+                            + "/" + p.getVersion()));
+                }
+                return Response.status(200).entity(result.toString()).build();
+            } else{
+                return Response.status(200).entity("No petals have " + name
+                        + "in requirement").build();
+            }
+        } catch (NoEntityFoundException e) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
     }
+
 }
